81681e2acff0ec9052b60c5c1d1e3f87bd3ca1da
IngenieroRicardo <138132387+IngenieroRicardo@users.noreply.github.com>
2025-06-18 12:43:19 -0600

    Add files via upload

---
+++ go/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+    db "github.com/IngenieroRicardo/db/go"
+)
+
+func main() {
+    
+
+
+    connection := "root:123456@tcp(192.100.1.210:3306)/chat"
+    
+    //insertWithColumns := "SELECT NOW()"
+
+insertWithColumns := "CALL insertado(JSON[nickname,BLOB(picture)])"
+//insertWithColumns := "INSERT INTO chat.usuario(picture, foto, id, nickname) VALUES(JSON[BLOB(picture),BLOB(foto),id,nickname])"
+/*insertJson := `
+    {
+        "id": 1000,
+        "nickname": "test1000",
+        "picture": "iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAIAAABLbSncAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAArSURBVBhXY/iPA0AlGBgwGFAKlwQmAKrAIgcVRZODCsI5cAAVgVDo4P9/AHe4m2U/OJCWAAAAAElFTkSuQmCC",
+        "foto": null
+    }`*/
+insertJson := `[
+	{
+        "id": 1,
+        "nickname": "test1",
+        "picture": null,
+        "foto":"iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAIAAABLbSncAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAArSURBVBhXY/iPA0AlGBgwGFAKlwQmAKrAIgcVRZODCsI5cAAVgVDo4P9/AHe4m2U/OJCWAAAAAElFTkSuQmCC" 
+    },
+    {
+        "id": 2,
+        "nickname": "test2",
+        "picture": "iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAIAAABLbSncAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAArSURBVBhXY/iPA0AlGBgwGFAKlwQmAKrAIgcVRZODCsI5cAAVgVDo4P9/AHe4m2U/OJCWAAAAAElFTkSuQmCC",
+        "foto":"iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAIAAABLbSncAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAArSURBVBhXY/iPA0AlGBgwGFAKlwQmAKrAIgcVRZODCsI5cAAVgVDo4P9/AHe4m2U/OJCWAAAAAElFTkSuQmCC" 
+    },
+    {
+        "id": 3,
+        "nickname": "test3",
+        "picture": "iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAIAAABLbSncAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAArSURBVBhXY/iPA0AlGBgwGFAKlwQmAKrAIgcVRZODCsI5cAAVgVDo4P9/AHe4m2U/OJCWAAAAAElFTkSuQmCC",
+        "foto": null
+    }
+]`
+
+result := db.SQLrun("mysql", connection, insertWithColumns, insertJson)
+
+	fmt.Println(result)
+}
